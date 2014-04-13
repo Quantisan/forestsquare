f0ca9c3a067b9681a831e19f5b80f73148d94cc9
Paul Lam <paul@quantisan.com>
2014-04-12 21:29:19 -0400

    simple server endpoint seem working

---
+++ backend/Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --log-level=debug server:app
---
+++ backend/server.py
@@ -0,0 +1,27 @@
+from bottle import Bottle, run, request, response
+import evn
+import sys
+
+app = Bottle()
+
+# Should use a POST but easier for iOS app to send GET ... so whatever
+@app.get('/tree/<treeId>')
+def get_create_tree(treeId):
+    try:
+        height = request.query.height
+        radius = request.query.radius
+        locationX = request.query.locationX  ## UTM X-coordinate
+        locationY = request.query.locationY
+        species = request.query.species
+
+        guid = evn.recordTree(treeId, height, radius, locationX, locationY, species)
+
+        response.status = '200 OK'
+        return {'guid': guid}
+    except:
+        response.status = '500 Internal Server Error'
+        print "Unexpected error: ", sys.exc_info()[0]
+        return
+
+if __name__ == "__main__":
+    run(app, host='localhost', port=8080, reloader=True, debug=True)
